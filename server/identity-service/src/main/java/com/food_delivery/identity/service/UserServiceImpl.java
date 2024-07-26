b9d0d289889e33590ed7835ee8baf00bb5a1b2f2
Truong Bui <bntruong99@gmail.com>
2024-07-26 16:17:45 +0700

    ADD: discovery service with eureka

--- server/identity-service/src/main/java/com/food_delivery/identity/service/UserServiceImpl.java
+++ server/identity-service/src/main/java/com/food_delivery/identity/service/UserServiceImpl.java
@@ -14,6 +14,7 @@ import com.food_delivery.identity.mapper.UserMapper;
 import com.food_delivery.identity.repository.UserRepository;
 import com.food_delivery.identity.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -25,6 +26,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -40,17 +42,21 @@ public class UserServiceImpl implements UserService {
     public UserResponse signUp(UserSignUpRequest userSignUpRequest) {
         Optional<User> userOptional = userRepository.findByEmail(userSignUpRequest.getEmail());
         if (userOptional.isPresent()) {
+            log.warn("User with email {} already exists", userSignUpRequest.getEmail());
             throw new DuplicateResourceException(ErrorCode.ERR_USER_DUPLICATE);
         }
+
         var user = userMapper.toUser(userSignUpRequest);
         user.setPassword(passwordEncoder.encode(userSignUpRequest.getPassword()));
         user.setAuthProvider(AuthProvider.LOCAL);
         userRepository.save(user);
+        log.info("User with id {} created", user.getId());
 
         // Call profile service to create profile
         var profileRequest = profileServiceMapper.toProfileCreateRequest(userSignUpRequest);
         profileRequest.setUserId(user.getId());
         profileServiceClient.createProfile(profileRequest);
+        log.info("Profile created for user {}", user.getId());
 
         var accessToken = jwtService.generateJwtToken(user);
         var userResponse = userMapper.toUserResponse(user);
@@ -67,6 +73,8 @@ public class UserServiceImpl implements UserService {
                 )
         );
         User user = (User) authentication.getPrincipal();
+        log.info("Get user from authenticationManager: {}", user);
+
         var accessToken = jwtService.generateJwtToken(user);
         var userResponse = userMapper.toUserResponse(user);
         userResponse.setAccessToken(accessToken);
@@ -78,18 +86,22 @@ public class UserServiceImpl implements UserService {
         Optional<User> userOptional = userRepository.findByEmail(googleSignInRequest.getEmail());
         User user;
         if (userOptional.isEmpty()) {
+            log.warn("User with email {} not found, creating new user", googleSignInRequest.getEmail());
             AuthProvider provider = AuthProvider.valueOf(googleSignInRequest.getAuthProvider().toUpperCase());
             googleSignInRequest.setAuthProvider(String.valueOf(provider));
             User newUser = userMapper.toUser(googleSignInRequest);
             user = userRepository.save(newUser);
+            log.info("User with id {} created", user.getId());
 
             // Call profile service to create profile
             var profileRequest = profileServiceMapper.toProfileCreateRequest(googleSignInRequest);
             profileRequest.setUserId(user.getId());
             profileServiceClient.createProfile(profileRequest);
+            log.info("Profile created for user {}", user.getId());
 
         } else {
             user = userOptional.get();
+            log.info("User with id {} found", user.getId());
         }
         var accessToken = jwtService.generateJwtToken(user);
         var userResponse = userMapper.toUserResponse(user);
